8d891421deae9622750689b69349a99c63447552
captainozlem <ozlemniksch@gmail.com>
2019-01-25 11:40:02 -0600

    first map

--- src/screen/MapScreen.js
+++ src/screen/MapScreen.js
@@ -1,60 +1,23 @@
 import React, { Component } from 'react';
 import { Picker, StyleSheet, View, Button } from 'react-native';
+import MapView from 'react-native-maps';
 
-let events = [
-  {
-    name: 'Sport & Outdoor',
-    subcategories: ['Hiking', 'Gym', 'Picnic', 'Boxing'],
-  },
-  { name: 'Shopping', subcategories: ['Local Market', 'Outlet'] },
-  { name: 'Arts & Crafts', subcategories: ['Paint', 'Sewing', 'Poetry'] },
-  { name: 'Fitness & Wellness', subcategories: ['Yoga', 'Meditation'] },
-  { name: 'Foodventure', subcategories: ['Thai', 'Mediterranean'] },
-  { name: 'Local Discoveries', subcategories: ['Walking Tour', 'Museums'] },
-  { name: 'Entertainment', subcategories: ['Movie', 'Coffee', 'Theater'] },
-  { name: 'Create your own', subcategories: ['It is all up to you!'] },
-];
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    margin: 30,
-  },
-});
 
 export default class MapScreen extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     PickerSelectedVal: '';
-  //   }
-  // }
-  // getSelectedPickerValue = () => {
-  //   Alert.alert('I want to do this ' + this.state.PickerSelectedVal + ' today.')
-  // }
+
   render() {
-    // let category = events.map(item => {
-    //   return item.name;
-    // });
-    // let subcategory = events.map(item => {
-    //   return item.subcategories.map(sub => {
-    //     return sub;
-    //   });
-    // });
+
     return (
-      <View styles={styles.container}>
-        {events.map(event => {
-          return (
-            <Picker key={event.name} selectedValue={event.name}>
-              {event.subcategories.map(item => {
-                return <Picker.Item key={item} label={item} value={item} />;
-              })}
-            </Picker>
-          );
-        })}
-        <Button title="I want to do" onPress={this.category} />
-      </View>
+     <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 41.8781,
+          longitude: -87.6298,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+
+        }}
+      />
     );
   }
 }
