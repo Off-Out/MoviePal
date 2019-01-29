1332aff219a51863da4e94f283735f8d5a900da1
Morgan Crouch <morganloel@gmail.com>
2019-01-29 17:27:03 -0600

    merging

--- src/screen/LoginScreen.js
+++ src/screen/LoginScreen.js
@@ -50,30 +50,30 @@ class LoginScreen extends Component {
         }
       })
       .then(() => {
-        const user = auth.currentUser || {}
+        const user = auth.currentUser || {};
         if (user.uid) {
           database.ref(`users/${user.uid}`).once('value', snapshot => {
             if (snapshot.exists()) {
               console.log('exists!');
-              this.props.navigation.navigate('App', { userId: user.uid })
-            };
+              this.props.navigation.navigate('App', { userId: user.uid });
+            }
             if (!snapshot.exists()) {
-              console.log('doesnt exist!')
-              console.log("loginscreen user", user)
-              
+              console.log('doesnt exist!');
+              console.log('loginscreen user', user);
+
               database.ref(`users/${user.uid}`).set({
                 name: user.providerData[0].displayName,
                 email: user.providerData[0].email,
-                photo: user.providerData[0].photoURL
+                photo: user.providerData[0].photoURL,
               });
-              this.props.navigation.navigate('App', { userId: user.uid })
+              this.props.navigation.navigate('App', { userId: user.uid });
             }
           });
         }
       })
       .catch(error => console.error(error));
   };
-  
+
   signInWithFacebook = async () => {
     const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
       '367646357383853',
