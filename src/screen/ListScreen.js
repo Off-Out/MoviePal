e62fb7f9406acd6063ca586d66870d1111957c1c
captainozlem <ozlemniksch@gmail.com>
2019-01-29 17:05:48 -0600

    Theater shows as a list

--- src/screen/ListScreen.js
+++ src/screen/ListScreen.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
-import { Text,SafeAreaView, View ,Button} from 'react-native';
+import { Text,ScrollView, View} from 'react-native';
+import { Button, Card, Title, Paragraph } from 'react-native-paper';
 import MapScreen from './MapScreen';
+
+
 
 
 class ListScreen extends React.Component {
   render() {
-    /* 2. Get the param, provide a fallback value if not available */
+
     const { navigation } = this.props;
 
      const theaters = navigation.getParam('theaters');
 
 
     return (
-      <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>List Screen</Text>
-        <Text> Theaters: {JSON.stringify(theaters)}</Text>
+      <ScrollView>
+        <Title> Theaters  </Title>
+         {theaters.map(item => (<Text key={item.theaterId}>{item.name}</Text> ))}
+
+  {/* {
+    theaters.map((item, i) => {
+      return (
+       <Card key={i} >
+
+          <Card.Content >{item.name}</Card.Content >
+          <Card.Content > {item.location.distance}</Card.Content >
+           <Card.Content > {item.location.telephone}</Card.Content >
+        </Card>
+      );
+    })
+  } */}
+
+
 
 
 
@@ -22,7 +40,7 @@ class ListScreen extends React.Component {
           title="Go back"
           onPress={() => this.props.navigation.goBack()}
         />
-      </SafeAreaView>
+      </ScrollView>
     );
   }
 }
