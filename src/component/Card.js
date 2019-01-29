452887b99089002da10455d009ed3a1f5b06a5af
Morgan Crouch <morganloel@gmail.com>
2019-01-29 13:30:28 -0600

    added movie description with elipse cutoff

--- src/component/Card.js
+++ src/component/Card.js
@@ -4,80 +4,88 @@ import { Alert, View, Text, SafeAreaView } from 'react-native';
 
 class EventCard extends React.Component {
   shouldComponentUpdate(prevProps) {
-    return prevProps.state.genre !== this.props.state.genre;
+    return prevProps.state.movie !== this.props.state.movie;
   }
   render() {
-    if (this.props.image) {
-      return <Text>...Loading</Text>;
-    } else {
-      return (
-        <SafeAreaView>
-          <Card
-            style={{ alignItems: 'stretch', backgroundColor: 'white' }}
-            elevation={4}
+    return (
+      <SafeAreaView>
+        <Card style={{ backgroundColor: 'white' }} elevation={4}>
+          <View
+            style={{
+              flexDirection: 'row',
+              marginHorizontal: 40,
+              backgroundColor: 'white',
+            }}
           >
-            <View style={{ flexDirection: 'row', backgroundColor: 'white' }}>
-              <Card.Content style={{ marginTop: 25 }}>
-                {this.props.state.movie ? (
-                  <Title>{this.props.state.movie}</Title>
-                ) : (
-                  <Title>{this.props.state.user}</Title>
-                )}
+            <Card.Content style={{ marginTop: 25, marginRight: 40 }}>
+              {this.props.state.movie ? (
+                <Title>{this.props.state.movie}</Title>
+              ) : (
+                <Title>{this.props.state.user}</Title>
+              )}
 
-                {this.props.state.user ? (
-                  <Paragraph>USER</Paragraph>
-                ) : (
-                  <View>
-                    <Paragraph>
-                      {this.props.state.genre}
-                      {'   '}
-                      {this.props.state.rating}
-                    </Paragraph>
+              {this.props.state.user ? (
+                <Paragraph>{this.props.state.userEmail}</Paragraph>
+              ) : (
+                <View>
+                  <Paragraph style={{ alignSelf: 'right' }}>
+                    {this.props.state.genres}
+                    {'   '}
+                    {this.props.state.rating}
+                  </Paragraph>
 
-                    <Paragraph>{this.props.state.theater}</Paragraph>
-                  </View>
-                )}
-              </Card.Content>
-              <Card.Cover
-                style={{
-                  marginTop: 10,
-                  marginRight: 10,
-                  marginBottom: 10,
-                  width: 100,
-                  height: 100,
-                  backgroundColor: 'pink',
-                }}
-                source={{ uri: this.props.state.image }}
-              />
-            </View>
-            {this.props.state.user ? null : (
-              <Card.Actions>
-                <Button
-                  onPress={() =>
-                    Alert.alert(
-                      'Add to Calendar',
-                      'Calendar Options',
-                      [
-                        {
-                          text: 'Google Calendar',
-                          onPress: () => console.log('ADD TO GOOGLE CALENDAR'),
-                        },
-                        { text: 'Outlook Calendar' },
-                        { text: 'virtually have us write it in your planner' },
-                      ],
-                      { cancelable: false }
-                    )
-                  }
-                >
-                  Add to Calendar
-                </Button>
-                <Button>Back to Map</Button>
-              </Card.Actions>
-            )}
-          </Card>
-        </SafeAreaView>
-      );
-    }
+                  <Paragraph>{this.props.state.theater}</Paragraph>
+                  <Paragraph
+                    numberOfLines={5}
+                    ellipsizeMode="tail"
+                    style={{ width: 200 }}
+                  >
+                    {this.props.state.shortDescription}
+                  </Paragraph>
+                </View>
+              )}
+            </Card.Content>
+            <Card.Cover
+              style={{
+                alignContent: 'right',
+                marginTop: 10,
+                marginRight: 10,
+                marginBottom: 10,
+                width: 100,
+              }}
+              source={{
+                uri:
+                  'http://developer.tmsimg.com/assets/p14087450_v_v6_aa.jpg?api_key=w8xkqtbg6vf3aj5vdxmc4zjj',
+              }}
+            />
+          </View>
+          {this.props.state.user ? null : (
+            <Card.Actions style={{ justifyContent: 'center' }}>
+              <Button
+                onPress={() =>
+                  Alert.alert(
+                    'Add to Calendar',
+                    'Calendar Options',
+                    [
+                      {
+                        text: 'Google Calendar',
+                        onPress: () => console.log('ADD TO GOOGLE CALENDAR'),
+                      },
+                      { text: 'Outlook Calendar' },
+                      { text: 'virtually have us write it in your planner' },
+                    ],
+                    { cancelable: false }
+                  )
+                }
+              >
+                Add to Calendar
+              </Button>
+              <Button>Back to Map</Button>
+            </Card.Actions>
+          )}
+        </Card>
+      </SafeAreaView>
+    );
   }
 }
 
--- src/screen/SingleEvent.js
+++ src/screen/SingleEvent.js
@@ -56,7 +56,13 @@ const dummyShowTimes = [{ time: `515` }, { time: '6:15' }, { time: `715` }];
 class SingleEvent extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      movie: '',
+      genres: '',
+      rating: '',
+      showtime: [],
+      selectedTime: '',
+    };
     this.config = {
       url: 'http://data.tmsapi.com/v1.1/movies/showings',
       zipCode: '60647',
@@ -69,10 +75,11 @@ class SingleEvent extends React.Component {
 
   async componentDidMount() {
     this.setState({
+      userGenre: 'horror',
       movie: 'Glass',
       tmsId: 'MV010018040000',
       rootId: '13817',
-      genre: 'Thriller',
+      genres: 'Thriller',
       rating: 'PG-13',
       time: 'evening',
       theater: 'Logan Theatre',
@@ -90,8 +97,9 @@ class SingleEvent extends React.Component {
         longitude: '-87.7084',
       },
       theatreId: '2877',
-      showtime: ['11:00 AM', '1:00 PM', '3:15 PM'],
+      showtime: [],
       selectedTime: '',
+      shortDescription: '',
     });
 
     await this.fetchTheaters();
@@ -105,61 +113,80 @@ class SingleEvent extends React.Component {
     const showtimes = movies[0].showtimes.map(
       movie => movie.dateTime.split('T')[1]
     );
-    this.setState({ showtime: showtimes });
+    const rating = movies[0].ratings[0].code;
+    const genres = movies[0].genres.join('');
+    const image = movies[0].preferredImage.uri;
+    const shortDescription = movies[0].shortDescription;
 
-    console.log('MOVIES AT LOGAN SQUARE', showtimes);
+    this.setState({
+      showtime: showtimes,
+      rating: rating,
+      genres: genres,
+      shortDescription: shortDescription,
+      image: image,
+    });
+    console.log('SHORT DESCRIPTION', shortDescription);
   }
   handlePress(selectedTime) {
     this.setState({ selectedTime });
   }
   render() {
-    return (
-      <View
-        style={{
-          flexDirection: 'column',
-          flex: 1,
-          justifyContent: 'flex-bottom',
-          alignContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <View style={{ flex: 1 }}>
-          <EventCard state={this.state} />
-        </View>
-
-        <Title style={{ marginTop: 10 }}>Show Times</Title>
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-          {!this.state.selectedTime ? (
-            <FlatList
-              numColumns={3}
-              data={this.state.showtime}
-              renderItem={({ item }) => (
-                <Button
-                  mode="outlined"
-                  style={{
-                    flexDirection: 'center',
-                    height: 40,
-                    width: 110,
-                    margin: 10,
-                    marginEnd: 10,
-                  }}
-                  key={item}
-                  accessibilityLabel={item}
-                  onPress={() => this.handlePress(item)}
-                >
-                  {item}
-                </Button>
+    if (!this.state.shortDescription) {
+      return <Text>...Loading</Text>;
+    } else {
+      return (
+        <View
+          style={{
+            flexDirection: 'column',
+            flex: 1,
+            justifyContent: 'flex-bottom',
+            alignContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <View style={{ flex: 1 }}>
+            <EventCard state={this.state} />
+          </View>
+          <View style={{ flex: 1 }}>
+            <Title style={{ alignSelf: 'center', marginTop: 10 }}>
+              Show Times
+            </Title>
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+              {!this.state.selectedTime ? (
+                <FlatList
+                  numColumns={3}
+                  data={this.state.showtime}
+                  renderItem={({ item }) => (
+                    <Button
+                      mode="outlined"
+                      style={{
+                        flexDirection: 'center',
+                        height: 40,
+                        width: 110,
+                        margin: 10,
+                        marginEnd: 10,
+                      }}
+                      key={item}
+                      accessibilityLabel={item}
+                      onPress={() => this.handlePress(item)}
+                    >
+                      {item}
+                    </Button>
+                  )}
+                />
+              ) : (
+                <View>
+                  <Text style={{ margin: 10 }}>
+                    Select Tickets {`&`} Quantities
+                  </Text>
+                  <Button>Purchase Tickets!</Button>
+                </View>
               )}
-            />
-          ) : (
-            <Text>Purchase Tickets!</Text>
-          )}
+            </View>
+          </View>
         </View>
-        <View style={{ flex: 1, height: 60 }}>
-          <Text style={{ margin: 10 }}>Select Tickets {`&`} Quantities</Text>
-        </View>
-      </View>
-    );
+      );
+    }
   }
 }
 
