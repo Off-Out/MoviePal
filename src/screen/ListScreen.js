ea953b8d26f20f3f4321b24b3c56e25290de6623
captainozlem <ozlemniksch@gmail.com>
2019-01-29 19:45:52 -0600

    needs to improve the style

--- src/screen/ListScreen.js
+++ src/screen/ListScreen.js
@@ -13,36 +13,58 @@ class ListScreen extends React.Component {
 
      const theaters = navigation.getParam('theaters');
 
-
+    if (!theaters) {
+      return <Text>...Loading</Text>;
+    } else {
     return (
       <ScrollView>
         <Title> Theaters  </Title>
-         {theaters.map(item => (<Text key={item.theaterId}>{item.name}</Text> ))}
+         {/* {theaters.map(item => (<Text key={item.theaterId}>{item.name}</Text> ))} */}
 
-  {/* {
+    {
     theaters.map((item, i) => {
       return (
-       <Card key={i} >
+       <Card   style={{
+            backgroundColor: 'white',
+            width: 400,
+            height: 100,
+            alignItems: 'center',
+          }}
+          elevation={4} key={i} >
+          <View
+            style={{
+              flexDirection: 'row',
+              marginHorizontal: 40,
+              backgroundColor: 'white',
+            }}
+          >
+            <Card.Content style={{ marginTop: 25 }}>
 
-          <Card.Content >{item.name}</Card.Content >
-          <Card.Content > {item.location.distance}</Card.Content >
-           <Card.Content > {item.location.telephone}</Card.Content >
+          <Text >{item.name}</Text >
+          <Text > {item.location.distance}</Text >
+           <Text > {item.location.telephone}</Text>
+           </Card.Content>
+           </View>
+           <Card.Actions style={{ justifyContent: 'center' }}>
+            <Button
+                onPress={() =>
+                  Alert.alert(
+                    'You will see the selected theater page soon'
+                  )
+                }
+              >
+               <Text> Go to Theater Page</Text>
+              </Button>
+              </Card.Actions>
         </Card>
       );
     })
-  } */}
+  }
 
-
-
-
-
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
       </ScrollView>
     );
   }
+}
 }
 
 export default ListScreen;
