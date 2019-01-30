d38fc3b3d974fec084caa2f33328984d8befd07c
captainozlem <ozlemniksch@gmail.com>
2019-01-29 21:59:50 -0600

    looking better

--- src/screen/ListScreen.js
+++ src/screen/ListScreen.js
@@ -1,70 +1,65 @@
 import React, { Component } from 'react';
-import { Text,ScrollView, View} from 'react-native';
+import { Text, ScrollView, View, Alert } from 'react-native';
 import { Button, Card, Title, Paragraph } from 'react-native-paper';
 import MapScreen from './MapScreen';
 
-
-
-
 class ListScreen extends React.Component {
   render() {
-
     const { navigation } = this.props;
-
-     const theaters = navigation.getParam('theaters');
-
+    const theaters = navigation.getParam('theaters');
     if (!theaters) {
       return <Text>...Loading</Text>;
     } else {
-    return (
-      <ScrollView>
-        <Title> Theaters  </Title>
-         {/* {theaters.map(item => (<Text key={item.theaterId}>{item.name}</Text> ))} */}
-
-    {
-    theaters.map((item, i) => {
       return (
-       <Card   style={{
-            backgroundColor: 'white',
-            width: 400,
-            height: 100,
-            alignItems: 'center',
-          }}
-          elevation={4} key={i} >
-          <View
-            style={{
-              flexDirection: 'row',
-              marginHorizontal: 40,
-              backgroundColor: 'white',
-            }}
-          >
-            <Card.Content style={{ marginTop: 25 }}>
+        <ScrollView>
+          <Title> Theaters  </Title>
+          {/* {theaters.map(item => (<Text key={item.theaterId}>{item.name}</Text> ))} */}
 
-          <Text >{item.name}</Text >
-          <Text > {item.location.distance}</Text >
-           <Text > {item.location.telephone}</Text>
-           </Card.Content>
-           </View>
-           <Card.Actions style={{ justifyContent: 'center' }}>
-            <Button
-                onPress={() =>
-                  Alert.alert(
-                    'You will see the selected theater page soon'
-                  )
-                }
-              >
-               <Text> Go to Theater Page</Text>
-              </Button>
-              </Card.Actions>
-        </Card>
+          {
+            theaters.map((item, i) => {
+              const distance = item.location.distance.toString().slice(0, 4);
+              return (
+                <Card style={{
+                  backgroundColor: 'white',
+                  width: 400,
+                  height: 200,
+                  alignItems: 'left',
+                }}
+                  key={i} >
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      marginHorizontal: 40,
+                      backgroundColor: 'white',
+                    }}
+                  >
+                    <Card.Content style={{ marginTop: 25 }}>
+
+                      <Title >{item.name}</Title >
+                      <Text > {distance} miles away</Text >
+                      {item.location.telephone ? (<Text > Phone Number: {item.location.telephone}</Text>) : <Text> Limited information </Text>}
+
+                    </Card.Content>
+                  </View>
+                  <Card.Actions style={{ justifyContent: 'center' }}>
+                    <Button
+                      onPress={() =>
+                        Alert.alert(
+                          'You will see the selected theater page soon'
+                        )
+                      }
+                    >
+                      <Text> Go to Theater Page</Text>
+                    </Button>
+                  </Card.Actions>
+                </Card>
+              );
+            })
+          }
+        </ScrollView>
       );
-    })
+    }
   }
-
-      </ScrollView>
-    );
-  }
-}
 }
 
 export default ListScreen;
