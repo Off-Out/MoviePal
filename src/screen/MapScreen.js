fcb8d313b99ae8298fcf130c1cc2067fbbc38624
Duc Tran <gtductran@gmail.com>
2019-02-04 13:51:13 -0600

    working on map props

--- src/screen/MapScreen.js
+++ src/screen/MapScreen.js
@@ -141,19 +141,19 @@
 import React, { Component } from 'react';
 import { SafeAreaView } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
-import { Search } from '../component';
+//import { Search } from '../component';
 import { Ionicons } from '@expo/vector-icons';
 import axios from 'axios';
 import { connect } from 'react-redux';
 
 class MapScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      zipCode: '60606',
-      theaters: [],
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     zipCode: '60606',
+  //     theaters: [],
+  //   };
+  // }
 
   static navigationOptions = ({ navigation }) => {
     const theaters = navigation.getParam('theaters');
@@ -176,17 +176,17 @@ class MapScreen extends Component {
   };
 
   componentDidMount = async () => {
-    const response = await axios.get(
-      `http://data.tmsapi.com/v1.1/theatres?zip=${
-        this.state.zipCode
-      }&api_key=w8xkqtbg6vf3aj5vdxmc4zjj`
-    );
-    this.props.navigation.setParams({
-      theaters: response.data,
-    });
-    this.setState({
-      theaters: response.data,
-    });
+    // const response = await axios.get(
+    //   `http://data.tmsapi.com/v1.1/theatres?zip=${
+    //     this.state.zipCode
+    //   }&api_key=w8xkqtbg6vf3aj5vdxmc4zjj`
+    // );
+    // this.props.navigation.setParams({
+    //   theaters: response.data,
+    // });
+    // this.setState({
+    //   theaters: response.data,
+    // });
   };
 
   handleZipCodeChange = text => {
@@ -208,6 +208,7 @@ class MapScreen extends Component {
 
   render() {
     console.log('where is this data', this.props.theaters);
+
     return (
       <SafeAreaView style={{ flex: 1 }}>
         <MapView
@@ -221,28 +222,23 @@ class MapScreen extends Component {
           }}
           showsUserLocation
           showsMyLocationButton={true}
-        >
-          {this.state.theaters.map(marker => (
-            <Marker
-              key={marker.theatreId}
-              coordinate={{
-                latitude: parseFloat(marker.location.geoCode.latitude),
-                longitude: parseFloat(marker.location.geoCode.longitude),
-              }}
-              title={marker.name}
-              description={marker.location.address.street}
-              onPress={() =>
-                this.props.navigation.navigate('SingleTheater', {
-                  theatre: marker,
-                })
-              }
-            />
-          ))}
-        </MapView>
-        <Search
-          handleZipCodeChange={this.handleZipCodeChange}
-          handleZipCodeSubmit={this.handleZipCodeSubmit}
         />
+        {this.props.theaters.map(marker => (
+          <Marker
+            key={marker.theatreId}
+            coordinate={{
+              latitude: parseFloat(marker.location.geoCode.latitude),
+              longitude: parseFloat(marker.location.geoCode.longitude),
+            }}
+            title={marker.name}
+            description={marker.location.address.street}
+            onPress={() =>
+              this.props.navigation.navigate('SingleTheater', {
+                theatre: marker,
+              })
+            }
+          />
+        ))}
       </SafeAreaView>
     );
   }
