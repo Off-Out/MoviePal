1a7f18e417602c6bf73ca34a717567d031301d10
Morgan Crouch <morganloel@gmail.com>
2019-02-06 11:22:06 -0600

    merging

--- src/redux/app-redux.js
+++ src/redux/app-redux.js
@@ -16,7 +16,7 @@ const initialState = {
   loading: true,
   userID: null,
   zipCode: null,
-  favoriteAnimal: 'dog',
+  selectedMovie: [],
 };
 
 //
@@ -67,14 +67,14 @@ export const setZipCode = zipcode => {
 export const fetchTheaters = theaterID => {
   return async dispatch => {
     const theaterInfo = theaterID.map(async id => {
-      try{
+      try {
         // const { data: theater } = await axios.get(
         //   `http://data.tmsapi.com/v1.1/theatres/${id}?api_key=w8xkqtbg6vf3aj5vdxmc4zjj`
         // );
         // return theater;
-        } catch(error) {
-          console.error(error)
-        }
+      } catch (error) {
+        console.error(error);
+      }
     });
 
     const theaterDetails = await Promise.all(theaterInfo);
@@ -83,15 +83,14 @@ export const fetchTheaters = theaterID => {
 };
 
 export const fetchNearbyTheaters = (lat, long) => {
-
   return async dispatch => {
     try {
-    // const { data: theaters } = await axios.get(
-    //   `http://data.tmsapi.com/v1.1/theatres?lat=${lat}&lng=${long}&api_key=w8xkqtbg6vf3aj5vdxmc4zjj`
-    // );
-    // dispatch(setTheaters(theaters));
-    } catch(error) {
-      console.error(error)
+      // const { data: theaters } = await axios.get(
+      //   `http://data.tmsapi.com/v1.1/theatres?lat=${lat}&lng=${long}&api_key=w8xkqtbg6vf3aj5vdxmc4zjj`
+      // );
+      // dispatch(setTheaters(theaters));
+    } catch (error) {
+      console.error(error);
     }
   };
 };
@@ -99,13 +98,13 @@ export const fetchNearbyTheaters = (lat, long) => {
 export const fetchMovies = (lat, long) => {
   return async dispatch => {
     try {
-    //   const { data: movies } = await axios.get(
-    //   `http://data.tmsapi.com/v1.1/movies/showings?startDate=${
-    //     initialState.date
-    //   }&lat=${lat}&lng=${long}&imageSize=Sm&api_key=w8xkqtbg6vf3aj5vdxmc4zjj`
-    // );
-    } catch(error) {
-      console.error(error)
+      //   const { data: movies } = await axios.get(
+      //   `http://data.tmsapi.com/v1.1/movies/showings?startDate=${
+      //     initialState.date
+      //   }&lat=${lat}&lng=${long}&imageSize=Sm&api_key=w8xkqtbg6vf3aj5vdxmc4zjj`
+      // );
+    } catch (error) {
+      console.error(error);
     }
     dispatch(setMovies(movies));
   };
